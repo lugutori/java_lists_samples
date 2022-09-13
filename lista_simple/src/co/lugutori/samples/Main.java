8edd78b4ddfcb7aabd1debccd1f8980988053ec7
Luis Guillermo Torres Ribero <lugutori@gmail.com>
2022-09-13 17:06:17 -0400

    Adding calls to MyList

--- lista_simple/src/co/lugutori/samples/Main.java
+++ lista_simple/src/co/lugutori/samples/Main.java
@@ -21,6 +21,45 @@ class Main {
         intList.addNode(4);
 
         System.out.println(intList);
+        //------------
+        MyList<Integer> list = new MyList<>();
+        list.add(4);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(9);
 
+        list.add(4);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(9);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+
+        list.removePos(5);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.removePos(7);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.removePos(2);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.removePos(0);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+
+        list.add(0,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(1,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(2,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(3,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(4,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(5,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(6,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
+        list.add(7,10);
+        System.out.println("list: " + list + " size: " + list.getSize() + " capacity: " + list.getCapacity());
     }
 }
